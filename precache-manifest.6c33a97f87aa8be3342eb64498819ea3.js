e73a5051db843b7fdfc94af0939e1d7c971411f5
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-19 10:15:32 -0700

    Updates

--- precache-manifest.6c33a97f87aa8be3342eb64498819ea3.js
+++ precache-manifest.6c33a97f87aa8be3342eb64498819ea3.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/autocomplete-search/static/js/runtime~main.61de82f9.js"
   },
   {
-    "revision": "f3bdd7317de1b0e06deb",
-    "url": "/autocomplete-search/static/js/main.f3bdd731.chunk.js"
+    "revision": "ad2269c39962408922b6",
+    "url": "/autocomplete-search/static/js/main.ad2269c3.chunk.js"
   },
   {
     "revision": "b75562ecba4416407294",
     "url": "/autocomplete-search/static/js/1.b75562ec.chunk.js"
   },
   {
-    "revision": "f3bdd7317de1b0e06deb",
+    "revision": "ad2269c39962408922b6",
     "url": "/autocomplete-search/static/css/main.ae0b8472.chunk.css"
   },
   {
-    "revision": "54b3aad063d24be737cd8f5c83ce87fc",
+    "revision": "3417b0b5d1a25d662952ddee27f3dbde",
     "url": "/autocomplete-search/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/autocomplete-search/precache-manifest.3d5da99e80c151f21ac3795edbe8cb27.js"
+  "/autocomplete-search/precache-manifest.6c33a97f87aa8be3342eb64498819ea3.js"
 );
 
 workbox.clientsClaim();
